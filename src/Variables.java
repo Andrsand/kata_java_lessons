e453c1dcb25f7d6a1755f98a70e6f7dbf82dbd7e
andrsand <angorko@list.ru>
2024-03-04 07:30:55 +0300

    First change

--- src/Variables.java
+++ src/Variables.java
@@ -1,8 +1,23 @@
 public class Variables {
     public static void main(String[] args) {
+        // целочисленные типы данных
         int myInt;
         myInt = 557;
-        short myShort = 3266; // тип для маленьких чисел
+        short myShort = 3266; // тип для маленьких чисел (16 ,bit)
+        long myLong = 326643252; // тип для ,больших чисел (64 ,bit)
+
+        // вещественные типы данных
+        double myDouble = 225.35; // 64 - bit
+        float myFloat = 2362.4f;  // 32 - bit  в кoнце ставится - f
+
+        // символ
+        char c = 'b';
+
+        // логический тип
+        boolean b = true;
+
+        byte myBite = 100; // вмещает только 8 bit т.е. 1 byte числа от - 128 - 128
+
         System.out.println(myInt);
     }
 }
