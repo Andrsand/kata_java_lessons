15a0558be3eca86605a008031335cca2cea594bb
andrsand <angorko@list.ru>
2024-03-04 07:18:36 +0300

    Initial commit

---
+++ src/Variables.java
@@ -0,0 +1,8 @@
+public class Variables {
+    public static void main(String[] args) {
+        int myInt;
+        myInt = 557;
+        short myShort = 3266; // тип для маленьких чисел
+        System.out.println(myInt);
+    }
+}
