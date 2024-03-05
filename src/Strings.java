bd15a9e66a8c47da273fe81bbeabac2e3adc95e4
andrsand <angorko@list.ru>
2024-03-05 04:32:49 +0300

    Reference data type

---
+++ src/Strings.java
@@ -0,0 +1,13 @@
+
+// Ссылочный тип данных String
+public class Strings {
+    public static void main(String[] args) {
+        int x = 5;
+        String s = "Hello"; // переменная s ссылается на объект класса String
+        String space = " ";
+        String name = "Bob";
+        System.out.println(s+space+name);
+        System.out.println("Hello"+" "+"Jone");
+        System.out.println("My namber is " + x);
+    }
+}
